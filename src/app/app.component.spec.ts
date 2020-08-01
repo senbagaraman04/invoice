6923882ad14e84e71292d6431949c94a5c0fd93b
Senbagaraman <senbagaraman04@gmail.com>
2020-08-01 22:25:54 +0530

    renamed to ramsBilling & added new component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'invoiceBilling'`, () => {
+  it(`should have as title 'ramsBilling'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('invoiceBilling');
+    expect(app.title).toEqual('ramsBilling');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('invoiceBilling app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ramsBilling app is running!');
   });
 });
