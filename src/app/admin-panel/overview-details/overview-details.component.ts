c664b79e2b472af6c7dca48f1a922c7466b2289f
Senbagaraman <senbagaraman04@gmail.com>
2020-08-06 21:04:24 +0530

    #3 added sideNav

--- src/app/admin-panel/overview-details/overview-details.component.ts
+++ src/app/admin-panel/overview-details/overview-details.component.ts
@@ -20,13 +20,23 @@ import * as XLSX from 'xlsx';
 export class OverviewDetailsComponent implements OnInit {
 
   
+  totalOrders: number = 10;
+  totalGinglyOil: number = 23.4;
+  totalCastorOil: number = 22.3;
+   totalCoconutOil : number = 92.3;
+
+   castorOilPrice: number =  243.3;
+   gingelyOilPrice: number = 323.8;
+   coconutPriceOil: number = 4874.3;
 
 
-  constructor( 
-           ) 
-  {
-    
-   }
+
+   coconutStockOil : number = 32;
+   gingelyStockOil: number = 34;
+   castorStockOil: number = 45;
+
+   
+  constructor() { }
 
   ngOnInit() {
   
