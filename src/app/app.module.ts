3e78a6bcfbfbcaaa9cb4fc7c433903c2359a3432
Senbagaraman <senbagaraman04@gmail.com>
2020-09-26 17:45:38 +0530

    rmbs:#5 sample invoice commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ const appRoutes: Routes = [
    {path: 'admin', component: AdminPanelComponent,
   children:[   
     {path: 'overview', component: OverviewDetailsComponent},
+    {path: 'invoice', component: InvoiceTemplateComponent}
   ]
   
 
