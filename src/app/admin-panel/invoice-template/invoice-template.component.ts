7c4811ac36515dc7ccf7603339cd808eb3f3b6b5
Senbagaraman <senbagaraman04@gmail.com>
2020-10-23 17:25:35 +0530

    fix: lint errors

--- src/app/admin-panel/invoice-template/invoice-template.component.ts
+++ src/app/admin-panel/invoice-template/invoice-template.component.ts
@@ -1,10 +1,10 @@
-import { Component,ChangeDetectorRef, OnInit } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 
-export class DynamicGrid{     
-  rate:number;  
-  quantity:number;  
-  description:string;  
+export class DynamicGrid {
+  rate: number;
+  quantity: number;
+  description: string;
 }
 
 
@@ -15,56 +15,56 @@ export class DynamicGrid{
 })
 export class InvoiceTemplateComponent implements OnInit {
 
-  constructor(private fb:FormBuilder,private cdr: ChangeDetectorRef) { }
+  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) { }
 
-  //private toastr: ToastrService
+  // private toastr: ToastrService
 
-  dynamicArray: Array<DynamicGrid> = [];  
-  newDynamic: any = {}; 
-  totalBilledAmount: number = 0;
-  
+  dynamicArray: Array<DynamicGrid> = [];
+  newDynamic: any = {};
+  totalBilledAmount = 0;
+
   ngOnInit() {
     this.cdr.detectChanges();
-    this.newDynamic = {name: "", quantity: "",rate:""};  
-    this.dynamicArray.push(this.newDynamic);  
+    this.newDynamic = {name: '', quantity: '', rate: '' };
+    this.dynamicArray.push(this.newDynamic);
 
   }
 
-  addRow() {    
-    this.newDynamic = {name: "", quantity: "",rate:""};  
-      this.dynamicArray.push(this.newDynamic);  
-       console.log(this.dynamicArray);  
-      return true;  
-  }  
+  addRow() {
+       this.newDynamic = {name: '', quantity: '', rate: ''};
+       this.dynamicArray.push(this.newDynamic);
+       console.log(this.dynamicArray);
+       return true;
+  }
 
-  deleteRow(index) {  
-      if(this.dynamicArray.length ==1) {  
-      
-           return false;  
-      } else {  
-          this.dynamicArray.splice(index, 1);  
-          //update the total billed amount when delete button clicked
-      this.calc(); 
-           return true;  
-      }  
-      
+  deleteRow(index) {
+      if (this.dynamicArray.length === 1) {
+
+           return false;
+      } else {
+          this.dynamicArray.splice(index, 1);
+          // update the total billed amount when delete button clicked
+          this.calc();
+          return true;
+      }
+
   }
 
 
   calc() {
-    console.log(this.dynamicArray);  
-   let totalSum = 0;
-    this.dynamicArray.forEach(e=>{
+    console.log(this.dynamicArray);
+    let totalSum = 0;
+    this.dynamicArray.forEach(e => {
      // console.log(e.rate);
      // console.log(e.quantity);
       totalSum = totalSum + ( e.rate * e.quantity);
-    })
+    });
 
     this.totalBilledAmount = totalSum;
   }
 
   addInv() {
-    
+     console.log(this.dynamicArray);
   }
 
 
