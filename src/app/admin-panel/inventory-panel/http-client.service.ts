473cadb0bb189f619bbf55572268e8b11975fe31
Senbagaraman <senbagaraman04@gmail.com>
2020-12-19 12:32:30 +0530

    feat: #11 added static stock list ui

---
+++ src/app/admin-panel/inventory-panel/http-client.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService {
+
+
+  constructor( private httpClient: HttpClient) { }
+
+
+  serverUrl = 'http://localhost:8080/api';
+
+  get(url: string): Observable<any> {
+      return this.httpClient.get(this.serverUrl + url);
+  }
+
+  // post(url: string, data: Invoice): Observable<any> {
+  //     return this.httpClient.post(this.serverUrl + url, data);
+  // }
+
+  // put(url: string, data: Invoice): Observable<any> {
+  //     return this.httpClient.put(this.serverUrl + url, data);
+  // }
+
+  // delete(url: string, data: Invoice): Observable<any> {
+  //     return this.httpClient.delete(this.serverUrl + url, { params: { id: data.id + '' } });
+  // }
+
+
+}
--- src/app/admin-panel/inventory-panel/inventory-panel.component.ts
+++ src/app/admin-panel/inventory-panel/inventory-panel.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { HttpClientService } from './http-client.service';
 
 @Component({
   selector: 'app-inventory-panel',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inventory-panel.component.scss']
 })
 export class InventoryPanelComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild('form', {static:true}) ngForm;
+  dateTime;
+  myForm: any;
+  stockList: any;
+  constructor( private formBuilder: FormBuilder,private httpClientService:HttpClientService) { }
 
   ngOnInit() {
+    this.dateTime = new Date();
+    this.myForm = this.formBuilder.group({
+      groudNutOnelt: new FormControl(this.stockList ? this.stockList.groudNutOnelt: '', Validators.required),
+      groudNuthalflt: new FormControl(this.stockList ? this.stockList.groudNuthalflt: '', Validators.required),
+      sesameOnelt: new FormControl(this.stockList ? this.stockList.sesameOnelt: '', Validators.required),
+      sesamehalflt: new FormControl(this.stockList ? this.stockList.sesamehalflt: '', Validators.required),
+      coconutOnelt:  new FormControl(this.stockList ? this.stockList.coconutOnelt: '', Validators.required),
+      coconuthalflt:  new FormControl(this.stockList ? this.stockList.coconuthalflt: '', Validators.required),
+      dateOfEntry: new FormControl(this.stockList ? new Date(): '', Validators.required),
+    });
   }
 
+
+  submitForm(formData) {
+       this.addStocks(formData.value);       
+  }
+
+  addStocks(stocks: stockListInterface) {
+      
+  }
+
+}
+
+
+export interface stockListInterface {
+  id: number;
+  groudNutOnelt: number;
+  groudNuthalflt: number;
+  sesameOnelt: number;
+  sesamehalflt: number;
+  coconutOnelt: number;
+  coconuthalflt: number;
+  dateOfEntry: number;
 }
